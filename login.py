815b092d6d4bc4e128018e98138ffdf12ac00cd5
HaSaN JON <hasanjon97@gmail.com>
2021-10-09 19:46:08 +0500

    Login and Password

---
+++ login.py
@@ -0,0 +1,41 @@
+
+import os
+import mysql.connector.connection
+
+
+class Login:
+    def __init__(self, name=None, login=None, password=None, age=None):
+        self.name = name
+        self.login = login
+        self.password = password
+        self.age = age
+
+    def choice(self):
+        pass
+
+    def registration(self):
+        pass
+
+    def login_(self):
+        pass
+
+    def change_login_and_password(self):
+        pass
+
+    def delete_account(self):
+        pass
+
+    def log_out(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
